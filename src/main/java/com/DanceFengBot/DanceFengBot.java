b89e89236ea0f8363a52401a5b0d8b4cef500309
Jing_Song <minecraftci66@qq.com>
2026-02-03 00:55:32 +0800

    edit plugin id

--- src/main/java/com/DanceFengBot/DanceFengBot.java
+++ src/main/java/com/DanceFengBot/DanceFengBot.java
@@ -34,7 +34,7 @@ public final class DanceFengBot extends JavaPlugin {
 
     private DanceFengBot() {
         super(new JvmPluginDescriptionBuilder( // 必要属性
-                        "com.DanceRinBot.DanceRinBot", // id
+                        "com.DanceFengBot.DanceFengBot", // id
                         "1.2" // version
                 ).author("Lin,Jingsong2008") // 可选属性，可以不提供， 直接 build
                         .build()

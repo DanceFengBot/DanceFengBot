6128534a2ef46cc9bdc516717d024abaf56b889d
逐花归海 <113447293+JingSong2008@users.noreply.github.com>
2026-02-25 02:24:41 +0800

    Update version number to 26.02.25

--- src/main/java/com/DanceFengBot/DanceFengBot.java
+++ src/main/java/com/DanceFengBot/DanceFengBot.java
@@ -29,7 +29,7 @@ public final class DanceFengBot extends JavaPlugin {
     private DanceFengBot() {
         super(new JvmPluginDescriptionBuilder( // 必要属性
                         "com.DanceFengBot.DanceFengBot", // id
-                        "1.3" // version
+                        "26.02.25" // version
                 ).author("Lin,Jingsong2008") // 可选属性，可以不提供， 直接 build
                         .build()
         );
